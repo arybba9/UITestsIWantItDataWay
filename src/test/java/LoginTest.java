6bbb42ba6ca43a911f3e76957de4d345118fbefc
Anna R <arybarczyk9@gmail.com>
2023-11-21 06:34:50 +0100

    Login

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -6,9 +6,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import test.java.pages.HomePage;
 import test.java.pages.LoginPage;
 
 import java.time.Duration;
+
 
 public class LoginTest {
     @Test
@@ -17,33 +19,21 @@ public class LoginTest {
         WebDriverManager.chromedriver().setup();
 
         WebDriver driver = new ChromeDriver();
-        //go to localhost
-        driver.get("http://localhost:3000");
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        //click on Login button
-        driver.findElement(By.xpath("//a[@class='btn btn-outline-secondary me-2']")).click();
+        driver.get("http://localhost:3000");
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         LoginPage login = new LoginPage();
-        //enter email address
-        LoginPage.email(driver).sendKeys("anna@kainos.com");
-        //enter password
-        LoginPage.password(driver).sendKeys("Test15!!");
-        //click Submit
-        driver.findElement(By.xpath("//*[@id='register']/div/form/button")).click();
+
+        login.clickOnLogInButton(driver);
+
+        login.enterCredentialsAndClickSubmit(driver, "anna@kainos.com","Test15!!" );
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        //click on Hello World
-        driver.findElement(By.partialLinkText("Hello World")).click();
-        //verify Hello World List
-        String str = driver.findElement(By.xpath("//*[@id='helloWorld']/h2")).getText();
-        Assert.assertEquals(str, "Hello World List");
-        //click on log out
-        driver.findElement(By.xpath("//a[@class='btn btn-outline-success me-2']")).click();
-        //click on Hello World
-        driver.findElement(By.partialLinkText("Hello World")).click();
-        // Verify if 'Login' is there
-        String log = driver.findElement(By.xpath("//h2[text()='Login']")).getText();
-        Assert.assertEquals(log, "Login");
+
+        HomePage home = new HomePage();
+        home.assertionHelloWorld(driver);
+
         driver.quit();
 
     }
@@ -52,28 +42,25 @@ public class LoginTest {
         WebDriverManager.chromedriver().setup();
 
         WebDriver driver = new ChromeDriver();
-        //go to localhost
+
         driver.get("http://localhost:3000");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         //click on Hello World
-        driver.findElement(By.partialLinkText("Hello World")).click();
+        driver.findElement(By.xpath("//a[@class='btn btn-outline-secondary me-2']")).click();
 
         LoginPage login = new LoginPage();
-        //enter email address
-        LoginPage.email(driver).sendKeys("anna@kainos.com");
-        //enter password
-        LoginPage.password(driver).sendKeys("Test15!");
-        //click on Submit
-        driver.findElement(By.xpath("//*[@id='register']/div/form/button")).click();
 
-        //assert the alert shows up
-        String str = driver.findElement(By.xpath("/html/body/main/section/div/div")).getText();
-        Assert.assertEquals(str, "The provided credentials could not be authenticated");
+        login.clickOnLogInButton(driver);
+
+        login.enterCredentialsAndClickSubmit(driver, "anna@kainos.com","Test15!" );
+
+        login.assertAlertCredentialCouldNotBeAuthenticated(driver,"The provided credentials could not be authenticated");
 
         driver.quit();
     }
     @Test
     public void invalidEmailValidPassword() {
+
         WebDriverManager.chromedriver().setup();
 
         WebDriver driver = new ChromeDriver();
@@ -81,23 +68,16 @@ public class LoginTest {
         driver.get("http://localhost:3000");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         //click on Hello World
-        driver.findElement(By.partialLinkText("Hello World")).click();
+        driver.findElement(By.xpath("//a[@class='btn btn-outline-secondary me-2']")).click();
 
         LoginPage login = new LoginPage();
         //enter email address
-        LoginPage.email(driver).sendKeys("anna.kainos.com");
-        //enter password
-        LoginPage.password(driver).sendKeys("Test15!!");
-        //click on submit
-        driver.findElement(By.xpath("//*[@id='register']/div/form/button")).click();
+        login.enterCredentialsAndClickSubmit(driver, "anna.kainos.com","Test15!!" );
 
-        //assert the alert shows up
-        String str = driver.findElement(By.xpath("/html/body/main/section/div/div")).getText();
-        Assert.assertEquals(str, "The provided credentials could not be authenticated");
+        login.assertAlertCredentialCouldNotBeAuthenticated(driver,"The provided credentials could not be authenticated");
 
         driver.quit();
     }
-
 
 }
 
--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -3,21 +3,45 @@ package test.java.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+import java.time.Duration;
 
 public class LoginPage {
-    // WebElements
-    public static WebElement email(WebDriver driver) {
+
+    private static WebElement logInButton(WebDriver driver){
+        return driver.findElement(By.xpath("//a[@class='btn btn-outline-secondary me-2']"));
+    }
+    private static WebElement emailField(WebDriver driver) {
         return driver.findElement(By.id("email"));
     }
-    public static WebElement password(WebDriver driver) {
+    private static WebElement passwordField(WebDriver driver) {
         return driver.findElement(By.id("password"));
     }
-    public static WebElement submit(WebDriver driver) {
+    private static WebElement submit(WebDriver driver) {
         return driver.findElement(By.xpath("//*[@id='register']/div/form/button"));
     }
+    private static WebElement alertCredentialCouldNotBeAuthenticated(WebDriver driver) {
+        return driver.findElement(By.xpath("/html/body/main/section/div/div"));
+    }
     //-----------------------------------------------------------------------------//
-    public void clickSubmit(WebDriver driver) {
+
+   public void clickOnLogInButton(WebDriver driver){
+        logInButton(driver).click();
+   }
+    public void enterCredentialsAndClickSubmit(WebDriver driver, String email, String password) {
+        emailField(driver).sendKeys(email);
+        passwordField(driver).sendKeys(password);
         submit(driver).click();
+
+    }
+    public void assertAlertCredentialCouldNotBeAuthenticated(WebDriver driver, String authenticationAlert) {
+        alertCredentialCouldNotBeAuthenticated(driver).getText();
+        Assert.assertEquals(authenticationAlert, "The provided credentials could not be authenticated");
+
+
     }
 }
+
+
 
