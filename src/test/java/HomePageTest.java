24c18ebac2a0bb38e141815fc5874905a5c1c678
Anna R <arybarczyk9@gmail.com>
2023-11-22 11:15:57 +0100

    Home Page

---
+++ src/test/java/HomePageTest.java
@@ -0,0 +1,47 @@
+package test.java;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import test.java.pages.BasePage;
+import test.java.pages.HomePage;
+import test.java.pages.LoginPage;
+
+import java.time.Duration;
+
+public class HomePageTest {
+        protected WebDriver driver;
+        @BeforeMethod
+        public void setupTest() {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+            driver.get("https://nphvsx5xpq.eu-west-1.awsapprunner.com/login");
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        }
+        @AfterMethod
+        public void cleanUpTest() {
+            driver.quit();
+        }
+        @Test
+        public void homePageTitle() {
+            LoginPage login = new LoginPage(driver);
+
+            login.clickOnLogInButton();
+
+            login.enterCredentialsAndClickSubmit("anna@kainos.com", "Test15!!");
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+            BasePage basePage = new BasePage(driver);
+            basePage.clickOnHomeLink();
+            HomePage homePage = new HomePage(driver);
+            homePage.getHomePageTitle();
+
+
+
+        }
+
+    }
+
--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -3,26 +3,29 @@ package test.java.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 
-public class HomePage {
+public class HomePage extends BasePage {
     // WebElements --------------------------------------------------------------------------
-    public static WebElement JobRoles(WebDriver driver) {
-
-        WebElement element = driver.findElement(By.linkText("Job Roles"));
-
-        return element;
+    public HomePage(WebDriver driver) {
+        super(driver);
     }
-
-//--------------------------------------------------------------------------------------------
-
-    // Click on these WebElements ---------------------------------------
-    public void clickLightning_Deals(WebDriver driver) {
-        JobRoles(driver).click();
+    private WebElement homePage() {
+        return this.driver.findElement(By.xpath("/html/body/main/div"));
     }
+    private WebElement helloWorld() {
+        return this.driver.findElement(By.partialLinkText("Hello World"));
+    }
+//......................................................................................//
+     public String getHomePageTitle() {
+        return homePage().getText(); }
 
-
-
-
+    public String getHelloWorldLink() {
+        return helloWorld().getText();
+    }
+    public void clickHelloWorld() {
+        helloWorld().click();
+    }
 
 }
 
